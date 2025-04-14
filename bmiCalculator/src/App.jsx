cad7219ab3b4f9cc87d83e5f9cd1dfbd0ae753f7
nligade78 <nikhilligade22@gmail.com>
2025-04-15 00:19:00 +0530

    Developed BMI Calculator In that understand useState ,onChange, HandleChange and clear button

---
+++ bmiCalculator/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import BmiCalculator from './components/BmiCalculator'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <BmiCalculator />
+    </>
+  )
+}
+
+export default App
---
+++ bmiCalculator/src/components/BmiCalculator.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+function BmiCalculator() {
+    const [weight, setWeight] = React.useState(0);
+    const [height, setHeight] = React.useState(0);
+    const [bmi, setBmi] = React.useState(0);
+
+    const weightSet = (e) => {
+        const weight = parseFloat(e.target.value);
+        setWeight(weight || 0);
+      };
+      
+
+      const heightSet = (e) => {
+        const height = parseFloat(e.target.value);
+        setHeight(height || 0);
+      };
+
+    const CalculateBMI = () => {
+        const heightInMeter = height * 0.0254;
+        const weightInKg = weight * 0.45359237;
+        let bmi = weightInKg / (heightInMeter * heightInMeter);
+        setBmi(bmi.toFixed(2));
+        console.log(bmi);
+    }
+
+    const handleReload = () => {
+        setWeight(0);
+        setHeight(0);
+        setBmi(0);
+      };
+      
+
+    
+  return (
+    <div className="container">
+      <div className="title">BMI Calculator</div>
+      <div className="input">
+        <span>Weight(lbs)</span>
+        <input type="number" placeholder="Enter Weight Value"  value={weight} onChange={weightSet}/>
+        <span>Height(in)</span>
+        <input type="number" placeholder="Enter Height Value"   value={height} onChange={heightSet}/>
+      </div>
+      <div className="buttons">
+        <button onClick={CalculateBMI}>Submit</button>
+        <button onClick={handleReload}>Reload</button>
+      </div>
+        <div className="result">
+            <span>Your BMI is: {bmi}</span>
+            <span>0</span>
+        </div>
+    </div>
+  );
+}
+
+export default BmiCalculator;
